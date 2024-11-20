0c2c85e744655af095220b14f06afe0d7fdcef0a
Ben-Flores <bflores133514@mymail.tstc.edu>
2024-11-20 17:27:50 -0600

    Create initial form, create required class files, add background image

--- SummitSportsApp/SummitSportsApp/Program.cs
+++ SummitSportsApp/SummitSportsApp/Program.cs
@@ -16,7 +16,7 @@ namespace SummitSportsApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmLogon());
         }
     }
 }
---
+++ SummitSportsApp/SummitSportsApp/frmLogon.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace SummitSportsApp
+{
+    partial class frmLogon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmLogon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.BackgroundImage = global::SummitSportsApp.Properties.Resources.bgLogon;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "frmLogon";
+            this.Text = "Log In";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
